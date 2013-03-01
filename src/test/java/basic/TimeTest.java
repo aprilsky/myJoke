ddf5aef1535aac5f5e064452b2197f2655ce6388
aprilsky <aprilsky5233@gmail.com>
2013-03-01 16:08:48 +0800

    时间比较

--- src/test/java/basic/TimeTest.java
+++ src/test/java/basic/TimeTest.java
@@ -11,16 +11,16 @@ import java.util.Date;
  */
 public class TimeTest {
     public static void main(String[] args) {
-        int syncHour = 1;
-        String uploadDate = "2013-02-28 15:08:50";
+        int syncHour = 24;
+        String uploadDate = "2013-03-01 16:02:43";
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         try {
             Date theDate =sdf.parse(uploadDate);
             Calendar calendar = Calendar.getInstance();
             calendar.setTime(theDate);
             calendar.add(Calendar.HOUR,syncHour);
-            System.out.println(syncHour+"小时后的上传时间："+sdf.format(calendar.getTime()));
-            System.out.println(calendar.getTime().after(new Date()));
+            System.out.println("上传时间后的"+syncHour+"小时："+sdf.format(calendar.getTime()));
+            System.out.println(new Date().after(calendar.getTime()));
         } catch (ParseException e) {
             e.printStackTrace();  //print Execption
         }
