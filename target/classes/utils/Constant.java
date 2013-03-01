6455dbe4beabc413b80834fdf8ca7be7dd94a948
aprilsky <aprilsky5233@gmail.com>
2013-03-01 13:10:26 +0800

    Revert "删除 target"
    This reverts commit 0400c0b8be09317c894073972579642c78e313c8.

---
+++ target/classes/utils/Constant.java
@@ -0,0 +1,19 @@
+package utils;
+
+/**
+ * @Author: caoxiao
+ * @Date: 12-11-23 下午1:47
+ */
+public class Constant {
+
+    public static final String offline =  "offline";
+
+    //在线
+    public static final String online   ="online";
+
+    public static final String session_user_key = "user";
+
+    public static final String article_status_wait_approve = "wait_approve";
+
+    public static final String article_status_passed = "passed";
+}
